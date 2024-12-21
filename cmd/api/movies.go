c128366b1d40abdfdcf097e3ab43d498356c00e3
hazbob <hrobinson3781@gmail.com>
2024-12-21 22:00:30 +0000

    validation of input of movie

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/hazbob/greenlight/internal/data"
+	"github.com/hazbob/greenlight/internal/validator"
 	"net/http"
 	"time"
 )
@@ -42,6 +43,20 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	if data.ValidateMovie(movie, v); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
