8a81f90901901b004744ade0dbb394d9b37383fc
hazbob <hrobinson3781@gmail.com>
2025-01-01 20:43:47 +0000

    add pagination to endpoints

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -148,11 +148,12 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 	WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) or $1 = '')
 	AND (genres @> $2 or $2 = '{}')
 	ORDER BY %s %s, id ASC
+	LIMIT $3 OFFSET $4
 `, filters.sortColumn(), filters.sortDirection())
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeoutTime)
 	defer cancel()
-
-	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
+	args := []interface{}{title, pq.Array(genres), filters.limit(), filters.offset()}
+	rows, err := m.DB.QueryContext(ctx, query, args...)
 	if err != nil {
 		return nil, err
 	}
