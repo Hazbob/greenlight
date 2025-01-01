be79f7be9f022f5ba281ccf091c32c11c1cb69f5
hazbob <hrobinson3781@gmail.com>
2025-01-01 19:22:48 +0000

    add sorting

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"github.com/hazbob/greenlight/internal/validator"
 	"github.com/lib/pq"
 	"time"
@@ -141,13 +142,13 @@ func (m MovieModel) Delete(id int64) error {
 }
 
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	query := `
+	query := fmt.Sprintf(`
 	SELECT id, created_at, title, year, runtime, genres, version
 	FROM movies
-	WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
+	WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) or $1 = '')
 	AND (genres @> $2 or $2 = '{}')
-	order by id;`
-
+	ORDER BY %s %s, id ASC
+`, filters.sortColumn(), filters.sortDirection())
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeoutTime)
 	defer cancel()
 
