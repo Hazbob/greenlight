236c241e527a85cea54e13cd19c890156c1456f5
hazbob <hrobinson3781@gmail.com>
2025-01-01 21:05:36 +0000

    added useful page metadata to the get all movies endpoint

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -177,12 +177,12 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 	if data.ValidateFilters(v, input.Filters); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 	}
-	movies, err := app.models.Movies.GetAll(input.Title, input.Genres, input.Filters)
+	movies, metadata, err := app.models.Movies.GetAll(input.Title, input.Genres, input.Filters)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
 	}
-	err = app.writeJson(w, http.StatusOK, envelope{"movies": movies}, nil)
+	err = app.writeJson(w, http.StatusOK, envelope{"metadata": metadata, "movies": movies}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
